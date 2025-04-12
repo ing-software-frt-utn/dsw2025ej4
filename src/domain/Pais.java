780ab9cd88a21f10009dccdfa9339312e13818c0
angelfadda <Angel.GilFadda@alu.utn.frt.edu.ar>
2025-04-12 00:33:05 -0300

    se agrego la clase Pais.java

---
+++ src/domain/Pais.java
@@ -0,0 +1,18 @@
+
+public class Pais {
+    private String nombre;
+    private String codigoISO;
+
+    public Pais(String nombre, String codigoISO) {
+        this.nombre = nombre;
+        this.codigoISO = codigoISO;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCodigoISO() {
+        return codigoISO;
+    }
+}
