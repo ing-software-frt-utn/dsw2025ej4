15e40e51fae3f5c05d1ddc1582a15747d1ece109
angelfadda <Angel.GilFadda@alu.frt.utn.edu.ar>
2025-04-13 22:25:16 -0300

    Update Persistencia.java

--- src/data/Persistencia.java
+++ src/data/Persistencia.java
@@ -12,15 +12,15 @@ public class Persistencia {
     private static ArrayList<Pais> paises = new ArrayList<>();
 
     private static void inicializarEspecies() {
-        especies.add(new Especie("León", TipoAlimentacion.CARNIVORO, 0.2));
+        especies.add(new Especie("LeÃ³n", TipoAlimentacion.CARNIVORO, 0.2));
         especies.add(new Especie("Jirafa", TipoAlimentacion.HERBIVORO, 0));
         especies.add(new Especie("Tigre", TipoAlimentacion.CARNIVORO, 0.15));
         especies.add(new Especie("Elefante", TipoAlimentacion.HERBIVORO, 0));
     }
 
     private static void inicializarSectores() {
-        Empleado raul = new Empleado("Raul A", "20111222", "Tucumán" );
-        Empleado maria = new Empleado("Maria B", "30111222", "Tucumán" );
+        Empleado raul = new Empleado("Raul A", "20111222", "TucumÃ¡n" );
+        Empleado maria = new Empleado("Maria B", "30111222", "TucumÃ¡n" );
         sectores.add(new Sector(1, -26.250724, -65.522827, 10, TipoAlimentacion.HERBIVORO, raul));
         sectores.add(new Sector(2, -26.252359, -65.521511, 10, TipoAlimentacion.CARNIVORO, maria));
         sectores.add(new Sector(3, -26.254661, -65.522726, 10, TipoAlimentacion.HERBIVORO, maria));
@@ -49,6 +49,10 @@ public class Persistencia {
         inicializarAnimales();
     }
 
+    public static ArrayList<Pais> getPaises() {
+    return paises;
+}    
+    
     public static ArrayList<Mamifero> getAnimales() {
         return animales;
     }
@@ -61,12 +65,6 @@ public class Persistencia {
         return especies;
     }
 
-    public static ArrayList<Pais> getPaises() {
-        return paises;
-    }
-
-    
-    
     public static double getTotalComida(TipoAlimentacion tipoAlimentacion) {
         double total = 0;
         for(Mamifero animal : animales){
@@ -74,4 +72,7 @@ public class Persistencia {
         }
         return total;
     }
+    
+
+      
 }
