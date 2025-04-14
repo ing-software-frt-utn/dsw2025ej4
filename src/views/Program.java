dedef44926191d47afacd99d95a20e1ad2eee56b
angelfadda <Angel.GilFadda@alu.frt.utn.edu.ar>
2025-04-13 22:55:00 -0300

    Update Program.java

--- src/views/Program.java
+++ src/views/Program.java
@@ -9,13 +9,11 @@ import java.util.InvalidPropertiesFormatException;
 public class Program {
 
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
-     /*   Persistencia.inicializar();
-        ListarAnimalesView view = new ListarAnimalesView();
-        view.setVisible(true);
-     */
+    
       Controlador c = new Controlador();
       
       c.ejecutar();
        
 }
 }
+
