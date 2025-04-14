9826e3f528b5ad666f851eb68f98d0ee4a879e50
angelfadda <Angel.GilFadda@alu.frt.utn.edu.ar>
2025-04-13 22:49:16 -0300

    Update InterfaceLista.java

--- src/views/InterfaceLista.java
+++ src/views/InterfaceLista.java
@@ -1,18 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package views;
 
-/**
- *
- * @author userr
- */
+import java.util.ArrayList;
+
 public interface InterfaceLista {
     public static String BTN_MOSTRAR = "pantalla lista boton mostrar";
     public static String BTN_VOLVER = "pantalla lista boton volver";
     
     public void ejecutar();
     public void volver();
-    public void setControlador(controlador.Controlador c);
+    public void setListaConfig(ArrayList<Object[]> lista);
+    public void setLista(ArrayList <AnimalViewModel> listaf);
+    public void setControlador(controlador.Controlador c); 
+    
 }
