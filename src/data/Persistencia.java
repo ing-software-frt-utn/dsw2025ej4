3e080a50111f41388723759daa91171dd2f15f6d
isaias romano <isaias.romano99@gmail.com>
2025-04-14 18:28:38 -0300

    agregado feature/alta-animal

--- src/data/Persistencia.java
+++ src/data/Persistencia.java
@@ -27,10 +27,10 @@ public class Persistencia {
     }
     
     private static void inicializarAnimales() throws InvalidPropertiesFormatException {
-        animales.add(new Carnivoro(5,250,especies.get(0), sectores.get(1)));
-        animales.add(new Carnivoro(2,180,especies.get(2), sectores.get(3)));
-        animales.add(new Herbivoro(3, 1020,especies.get(1), sectores.get(0), 170));
-        animales.add(new Herbivoro(8, 3800,especies.get(3), sectores.get(2), 320));
+//        animales.add(new Carnivoro(5,250,especies.get(0), sectores.get(1)));
+//        animales.add(new Carnivoro(2,180,especies.get(2), sectores.get(3)));
+//        animales.add(new Herbivoro(3, 1020,especies.get(1), sectores.get(0), 170));
+//        animales.add(new Herbivoro(8, 3800,especies.get(3), sectores.get(2), 320));
     }
 
     public static void inicializar() throws InvalidPropertiesFormatException{
